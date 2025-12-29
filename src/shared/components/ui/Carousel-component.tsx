4d5eaa7efff09f38f11016a5b4fba0d9e1777e72
Ob17oo <liemil534@gmail.com>
2025-12-29 05:00:46 +0300

    FinishCarouselComponent

--- src/shared/components/ui/Carousel-component.tsx
+++ src/shared/components/ui/Carousel-component.tsx
@@ -3,7 +3,6 @@
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel"
 import Image from "next/image"
 import { useRouter } from "next/navigation"
-import { useRef, useState } from "react"
 
 type SizeVariant = 'default' | 'medium' | 'large'
 
@@ -13,7 +12,8 @@ interface DataType {
     price?: number,
     image_url: string,
     launcher?: string,
-    launcher_url: string
+    launcher_url: string,
+    description?: string
 }
 
 interface CarouselProps{
@@ -27,11 +27,21 @@ export function CarouselComponent({carouselItem, carouselHeader, carouselImage, 
     const router = useRouter()
     const limitItem = carouselItem.slice(0,12)
     const sizeConfig = {
-        default: 'w-[15%]',
-        medium: '',
-        large: 'w-[50%]'
-    }
+        default: {
+            height: 'h-76',
+            cardBasis: 'basis-1/7'
 
+        },
+        medium: {
+            height: 'h-76',
+            cardBasis: 'basis-1/4'
+        },
+        large: {
+            height: 'h-96',
+            cardBasis: 'basis-1/3'
+        }
+
+    }
 
     return ( 
         <Carousel className="bg-secondary rounded-2xl p-4 mb-4" opts={{
@@ -40,35 +50,42 @@ export function CarouselComponent({carouselItem, carouselHeader, carouselImage, 
         }}>
             <div className="flex flex-col gap-3">
                 <div className="flex justify-between items-center">
-                    <div className="flex items-center gap-2">
-                        <div className="flex items-center justify-center w-8 h-8 bg-accent rounded-full">
+                     <div className="flex items-center gap-2">
+                        <div className="flex items-center justify-center w-10 h-10 bg-accent rounded-full">
                             {carouselImage && (
-                                <Image src={carouselImage} width={24} height={24} alt={carouselHeader}/>
+                                   <Image src={carouselImage} width={24} height={24} alt={carouselHeader}/>
                             )}
-                        </div>
+                            </div>
                         <h2 className="text-2xl ">{carouselHeader}</h2>
                     </div>
                     <div className="flex items-center gap-3 relative">
                         <CarouselPrevious />
-                        <CarouselNext />
+                         <CarouselNext />
                     </div>
                 </div>
-                <CarouselContent className="">
+                <CarouselContent className="flex gap-2">
                     { limitItem.map((item) => (
-                        <CarouselItem className="basis-1/6 cursor-pointer" key={item.title} onClick={() => router.push(`/games/${item.id}`)} >
-                            <div className="p-1">
-                            <div className="relative h-72 overflow-hidden rounded-2xl">
+                        <CarouselItem className={`cursor-pointer ${sizeConfig[sizeVariant].cardBasis}`} key={item.title} onClick={() => router.push(`/games/${item.id}`)} >
+                            <div className={`relative ${sizeConfig[sizeVariant].height} overflow-hidden rounded-2xl`}>
                                 <Image className="object-cover" fill src={item.image_url} alt={item.title || 'CarouselItem'} />
-                                <div className="absolute bottom-3 left-3 bg-[#6D6D6D]/60 rounded-2xl px-2 py-1">
-                                   <div className="flex items-center gap-2">
-                                        <Image src={item.launcher_url} width={24} height={24} alt={item.launcher || 'LauncherLogo'}/>
-                                        <p className="text-lg">{item.launcher}</p>
-                                   </div>
-                                </div>
+                                {sizeVariant === 'default' && (
+                                    <div className="absolute bottom-3 left-3 bg-[#6D6D6D]/60 rounded-2xl px-2 py-1">
+                                        <div className="flex items-center gap-2">
+                                                <Image src={item.launcher_url} width={24} height={24} alt={item.launcher || 'LauncherLogo'}/>
+                                                <p className="text-lg">{item.launcher}</p>
+                                        </div>
+                                    </div>
+                                )}
                             </div>
-                                <span className="text-lg text-green-400 font-semibold">{item.price} руб</span>
-                                <p className="text-lg">{item.title}</p>
-                            </div>
+                                { sizeVariant === 'default' && (
+                                    <span className="text-lg text-green-400 font-semibold">{item.price} руб</span>
+                                )}
+                                <p className={`text-lg ${sizeVariant !== 'medium' ? 'text-left' : 'text-center'}`}>{item.title}</p>
+                                {
+                                    sizeVariant === 'large' && (
+                                        <p className="">{item.description}</p>
+                                    )
+                                }
                         </CarouselItem>
                     ))}
                 </CarouselContent>
--- src/views/home/ui/home-page.tsx
+++ src/views/home/ui/home-page.tsx
@@ -1,4 +1,4 @@
-import { BannerCarouselComponent, CarouselComponent } from "@/shared/components";
+import { CarouselComponent } from "@/shared/components";
 
 export  function HomePage(){
     const gameItem = [
@@ -99,32 +99,147 @@ export  function HomePage(){
             launcher_url: '/static/launcher-logos/Steam.svg'
         },
     ]
+    const wallets = [
+        {
+            id: 1,
+            title: 'First',
+            price: 100,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 2,
+            title: 'Second',
+            price: 80,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 3,
+            title: 'Third',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 4,
+            title: 'Fourth',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 5,
+            title: 'Five',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 6,
+            title: 'sixth',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 7,
+            title: 'seven',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 8,
+            title: 'eight',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 9,
+            title: 'nine',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 10,
+            title: 'ten',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 11,
+            title: 'eleven',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+        {
+            id: 12,
+            title: 'twelve',
+            price: 120,
+            image_url: '/images/Dota2.jpg',
+            launcher: 'Steam',
+            launcher_url: '/static/launcher-logos/Steam.svg',
+            description: 'qpewq]ewqpewqewqewq]epwqe[wqpewq][eqw'
+        },
+    ]
     const bannerItem = [
         {
             id: 1,
             title: 'first',
             image_url: '/images/Dota2.jpg',
+            description: 'orewqpewqpeowqeowqeqw[peoqwp[qoewqepqwoewq[pewoqe[pwqeowq[p'
         },
         {
             id: 2,
             title: 'second',
             image_url: '/images/Dota2.jpg',
+            description: 'orewqpewqpeowqeowqeqw[peoqwp[qoewqepqwoewq[pewoqe[pwqeowq[p'
         },
         {
             id: 3,
             title: 'third',
             image_url: '/images/Dota2.jpg',
+            description: 'orewqpewqpeowqeowqeqw[peoqwp[qoewqepqwoewq[pewoqe[pwqeowq[p'
         },
         {
             id: 4,
             title: 'fourth',
             image_url: '/images/Dota2.jpg',
+            description: 'orewqpewqpeowqeowqeqw[peoqwp[qoewqepqwoewq[pewoqe[pwqeowq[p'
         },
     ]
     return ( 
         <>
-            {/* <BannerCarouselComponent carouselItem={bannerItem} /> */}
             <CarouselComponent sizeVariant="default" carouselItem={gameItem} carouselHeader="Популярные игры" carouselImage="/static/carouselIcon/Games.svg" />
+            <CarouselComponent sizeVariant="medium" carouselItem={gameItem} carouselHeader="Популярные лаунчеры" carouselImage="/static/carouselIcon/Rocket.svg" />
+            <CarouselComponent sizeVariant="large" carouselItem={wallets} carouselHeader="Популярные лаунчеры" carouselImage="/static/carouselIcon/Rocket.svg" />
         </>
     )
 }
